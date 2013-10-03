1b02fbc833f79df77a15081afa17f188d5484978
Oliver Hader <oliver@typo3.org>
2013-10-03 17:44:49 +0200

    [TASK] Import version 0.4.2 from TYPO3 Extension Repository (TER)

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mc_googlesitemap".
+ *
+ * Auto generated 03-10-2013 17:43
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Google Sitemap for Pages and Contents',
+	'description' => 'XML Generator for Google\'s sitemaps , can be used for pages or contents. All sitemaps options available.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.4.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'tt_content,pages',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Maximo Cuadros [Gobernalia Global Net S.A - GrupoBBVA]',
+	'author_email' => 'maximo.cuadros@grupobbva.com',
+	'author_company' => 'Gobernalia Global Net S.A (GrupoBBVA)',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'php' => '3.0.0-',
+			'typo3' => '3.5.0-',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
